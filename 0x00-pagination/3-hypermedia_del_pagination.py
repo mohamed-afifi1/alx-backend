f34c5292e267a7a85ce8002435d19e7fa0c989d4
root <mohammed2004koki@gmail.com>
2024-10-28 23:12:46 +0300

    test

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Get hypermedia pagination for a given index and page size.
+        """
+        assert isinstance(index, int)
+        next_index = index + page_size
+        data = self.dataset()[index:next_index]
+        return {
+            "index": index,
+            "page_size": page_size,
+            "next_index": next_index,
+            "data": data
+        }
