f7e5c85bf144445f821b9edc5070f102f027ef39
root <mohammed2004koki@gmail.com>
2024-10-31 01:52:49 +0300

    basic

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,14 +8,14 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    def put(self, key, item):
+    def put(self, key: str, item: str) -> None:
         """
         Put item into cache
         """
         if key is not None and item is not None:
             self.cache_data[key] = item
 
-    def get(self, key):
+    def get(self, key: str) -> str:
         """
         Get item from cache
         """
